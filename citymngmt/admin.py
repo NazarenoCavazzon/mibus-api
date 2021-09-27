3194658637e9091447d962638ab95769583767cc
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-09-27 00:55:41 -0300

    Changes

--- citymngmt/admin.py
+++ citymngmt/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-class CityAdmin(admin.ModelAdmin):
+class CityAdmin(admin.AdminSite):
     site_header = 'City Management'
 
-city_site = CityAdmin(name='BlogAdmin')
+city_site = CityAdmin(name="CityAdmin")
 city_site.register(models.City)
 
 
--- mibus/urls.py
+++ mibus/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.urls.conf import re_path
+from citymngmt.admin import city_site
 import mibus.views as views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
 
 urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', city_site.urls, name='admin'),
     path('login', views.login_view, name='login'),
     path('register', views.register_view, name='register'),
     path('register/company', views.register_company_view, name='register_company'),
