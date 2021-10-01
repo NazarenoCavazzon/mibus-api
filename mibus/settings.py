7328428eb5f8655c250ae9991326f9c804e57908
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-09-30 21:10:31 -0300

    Heroku Database
    Updated the database and requirements

--- mibus/settings.py
+++ mibus/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'mibus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mibus_app',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'PASSWORD': 'vicius438'
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+        'PASSWORD': config('PASSWORD')
     }
 }
 import dj_database_url
